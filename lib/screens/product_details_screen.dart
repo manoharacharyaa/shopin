998254a302dee74fd87235edc2474a81711f9e91
Manohar Acharya <manoharacharya63@gmail.com>
2024-02-09 15:31:22 +0530

    4.refractoring firstWhere logic in provider

--- lib/screens/product_details_screen.dart
+++ lib/screens/product_details_screen.dart
@@ -11,12 +11,12 @@ class ProductDetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     //extract productId from named route
     final productId = ModalRoute.of(context)?.settings.arguments as String;
-    final loadedProduct = Provider.of<Products>(context)
-        .items
-        .firstWhere((prod) => prod.id == productId);
+    final loadedProduct = context.read<Products>().findById(productId);
+//  final loadedProduct = Provider.of<Products>(context, listen: false).findById(productId);
     return Scaffold(
       appBar: AppBar(
         title: Text(loadedProduct.title),
+        // title: Text(productId),
       ),
       body: Image.asset(loadedProduct.imageUrl),
     );
