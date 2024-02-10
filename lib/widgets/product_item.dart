7adac3ee6d68325f4e8683e3d7fdc33c046339b6
Manohar Acharya <manoharacharya63@gmail.com>
2024-02-10 18:48:28 +0530

    6.using Conusmer

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -9,9 +9,11 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
-//  final product = context.watch<Product>();
+    // final product = Provider.of<Product>(context);
+    final product = context.read<Product>();
     final logger = Logger();
+    logger.d('Procuct Rebuilds');
+    logger.d(product.title);
 
     return InkWell(
       onTap: () {
@@ -20,20 +22,21 @@ class ProductItem extends StatelessWidget {
           arguments: product.id,
         );
         logger.d(product.id);
-        print(product.title);
       },
       child: ClipRRect(
         borderRadius: BorderRadius.circular(10),
         child: GridTile(
           footer: GridTileBar(
             backgroundColor: Colors.black45,
-            leading: IconButton(
-              onPressed: () {
-                product.toggleFavouriteStatus();
-              },
-              icon: product.isFavourite
-                  ? const Icon(Icons.favorite)
-                  : const Icon(Icons.favorite_border),
+            leading: Consumer<Product>(
+              builder: (context, product, child) => IconButton(
+                onPressed: () {
+                  product.toggleFavouriteStatus();
+                },
+                icon: product.isFavourite
+                    ? const Icon(Icons.favorite)
+                    : const Icon(Icons.favorite_border),
+              ),
             ),
             title: Text(product.title),
             trailing: IconButton(
